ac1c3b171a6aecf4820aa1210c298067f6c7df25
Samuel Felipe Camargo López <samfemcamlop@gmail.com>
2026-05-07 13:09:41 +0200

    Creacion Componentes

---
+++ src/app/components/team-form/team-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-form',
+  imports: [],
+  templateUrl: './team-form.html',
+  styleUrl: './team-form.css',
+})
+export class TeamForm {}
